c3eb40afdbadce889d3d92c65a3663763b4c4c5f
Anas Youssef El Mahdad <anasyoussef.elmahdad@proton.me>
2024-11-25 21:39:45 +0100

    treesitter

---
+++ after/plugin/nvim-treesitter.rc.lua
@@ -0,0 +1,29 @@
+local ok, configs = pcall(require, 'nvim-treesitter.configs')
+if not ok then
+    return vim.notify('COULD NOT LOAD NVIM-TREESITTER [CONFIGS]', vim.log.levels.ERROR, { title = 'NVIM-TREESITTER' })
+end
+
+local ensure_installed = {
+    'css',
+    'editorconfig',
+    'gitignore',
+    'html',
+    'javascript',
+    'json',
+    'jsonc',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'rust',
+    'toml',
+    'tsx',
+    'typescript',
+}
+
+configs.setup {
+    ensure_installed = ensure_installed,
+    sync_install = false,
+    auto_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+}
